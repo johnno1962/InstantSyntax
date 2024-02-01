ee93678d901c96617e43ff940aa6662334a8f7a4
John Holdsworth <>
2024-02-01 13:21:40 +0100

    Correct "Current" link in frameworks.

--- Package.swift
+++ Package.swift
@@ -51,85 +51,85 @@ let package = Package(
     .binaryTarget(
         name: "SwiftBasicFormat",
         url: "https://github.com/johnno1962/InstantSyntax/raw/main/509.1.1/SwiftBasicFormat.framework.xcframework.zip",
-        checksum: "1a46504376d662c419617e6423207e236fd313834cc8eaddf38bc24d00e98d5a"
+        checksum: "9478174e692363f2ec3e719e459bd6ae7278fbffe2009eacff3669a7062ae49e"
     ),
     // .library(name: "SwiftCompilerPlugin", targets: ["SwiftCompilerPlugin"]),
     .binaryTarget(
         name: "SwiftCompilerPlugin",
         url: "https://github.com/johnno1962/InstantSyntax/raw/main/509.1.1/SwiftCompilerPlugin.framework.xcframework.zip",
-        checksum: "a41af23effd566cd5fde1d367b7cfa3b9c7325a6cb49cbc430142fe37f586058"
+        checksum: "953cf791f14bba72574d26a8020d0d9823554d8e7d70c550153a151b954d68aa"
     ),
     // .library(name: "SwiftCompilerPluginMessageHandling", targets: ["SwiftCompilerPluginMessageHandling"]),
     .binaryTarget(
         name: "SwiftCompilerPluginMessageHandling",
         url: "https://github.com/johnno1962/InstantSyntax/raw/main/509.1.1/SwiftCompilerPluginMessageHandling.framework.xcframework.zip",
-        checksum: "a1dee1eacbf26ae36942fab79b2dcf42705b7923ce0b9ea0611d00576eebb5db"
+        checksum: "75e63a5015af6a4f8a46885df91dbf4fc070089747efc79b771b640f5547b231"
     ),
     // .library(name: "SwiftDiagnostics", targets: ["SwiftDiagnostics"]),
     .binaryTarget(
         name: "SwiftDiagnostics",
         url: "https://github.com/johnno1962/InstantSyntax/raw/main/509.1.1/SwiftDiagnostics.framework.xcframework.zip",
-        checksum: "710cbc008e8ab290236979df3430364a2c07112510d10c689ef05c1e380ee2af"
+        checksum: "d74548cb3d322dfb1ddc5c42611508f1b581e0febe98334076aaca8b7a4f8209"
     ),
     // .library(name: "SwiftOperators", targets: ["SwiftOperators"]),
     .binaryTarget(
         name: "SwiftOperators",
         url: "https://github.com/johnno1962/InstantSyntax/raw/main/509.1.1/SwiftOperators.framework.xcframework.zip",
-        checksum: "90a0213c8b5ab2e5096604f75ba232d871fd1481cb697f3c910b35be0b0b8224"
+        checksum: "60f5e7202f2d7ee95cb48f22c972999f0f3c473c8dd651e3b7bc98e93508510a"
     ),
     // .library(name: "SwiftParser", targets: ["SwiftParser"]),
     .binaryTarget(
         name: "SwiftParser",
         url: "https://github.com/johnno1962/InstantSyntax/raw/main/509.1.1/SwiftParser.framework.xcframework.zip",
-        checksum: "d5d474c7d817315656b5336a53c83ec5be52ed0ee65b3b0b5d618feacb55abb5"
+        checksum: "135225b16c01558211009f3d688886161fdd40466b6ca61d48b8f5afd67f5055"
     ),
     // .library(name: "SwiftParserDiagnostics", targets: ["SwiftParserDiagnostics"]),
     .binaryTarget(
         name: "SwiftParserDiagnostics",
         url: "https://github.com/johnno1962/InstantSyntax/raw/main/509.1.1/SwiftParserDiagnostics.framework.xcframework.zip",
-        checksum: "3fdfe2e2b3c5104d19875d5770bfddaf4fb8c351e0b798397f94291ea5faa992"
+        checksum: "eb9d444a5c2bd3e3a75b589748a6bf7d100e53f90e05197011db28bc9cb90d73"
     ),
     // .library(name: "SwiftSyntax", targets: ["SwiftSyntax"]),
     .binaryTarget(
         name: "SwiftSyntax",
         url: "https://github.com/johnno1962/InstantSyntax/raw/main/509.1.1/SwiftSyntax.framework.xcframework.zip",
-        checksum: "c659cced33e3bcd7656c16708db404e69f2f826ff6f2cd0ef5fb0c96a1f63955"
+        checksum: "ed1f5a65ba9880fc11937a38b429234e738ede9bba19f92b005658cb778cb7a0"
     ),
     // .library(name: "SwiftSyntax509", targets: ["SwiftSyntax509"]),
     .binaryTarget(
         name: "SwiftSyntax509",
         url: "https://github.com/johnno1962/InstantSyntax/raw/main/509.1.1/SwiftSyntax509.framework.xcframework.zip",
-        checksum: "4418e6b81c420dfcd6eb85f4da8994ed140ce132889d995a55e4d62da8b852ac"
+        checksum: "76b5c3f5b01ce5c75d5dff3026b9894169a2a0e435e313dbd35768ebeb092f1d"
     ),
     // .library(name: "SwiftSyntaxBuilder", targets: ["SwiftSyntaxBuilder"]),
     .binaryTarget(
         name: "SwiftSyntaxBuilder",
         url: "https://github.com/johnno1962/InstantSyntax/raw/main/509.1.1/SwiftSyntaxBuilder.framework.xcframework.zip",
-        checksum: "62989ed837d4288790d66bba4f086df7270ee3f312f233fcb3210dffa1a31672"
+        checksum: "fee97042e6fec0244083c2b9cc2603266cffdbea0ee16dc61d03313c7677faf0"
     ),
     // .library(name: "SwiftSyntaxMacroExpansion", targets: ["SwiftSyntaxMacroExpansion"]),
     .binaryTarget(
         name: "SwiftSyntaxMacroExpansion",
         url: "https://github.com/johnno1962/InstantSyntax/raw/main/509.1.1/SwiftSyntaxMacroExpansion.framework.xcframework.zip",
-        checksum: "0265aebaa51a1aa17cddcb45a795738b0b02e4611d4c577261ce2a8fe8ec45da"
+        checksum: "e576ac4cfbbdd9ecf64e0206a68a2e6679919b5bf9c9f114d11ebfdeb0cbe5dc"
     ),
     // .library(name: "SwiftSyntaxMacros", targets: ["SwiftSyntaxMacros"]),
     .binaryTarget(
         name: "SwiftSyntaxMacros",
         url: "https://github.com/johnno1962/InstantSyntax/raw/main/509.1.1/SwiftSyntaxMacros.framework.xcframework.zip",
-        checksum: "3975dec36f2cc6e78074561767a1ca127f03e3de5203e30f8f98f50c064c1bdd"
+        checksum: "b02e1e090fb4235cf83e9194de76900975b6c15effbfa39580085f93cadf5282"
     ),
     // .library(name: "SwiftSyntaxMacrosTestSupport", targets: ["SwiftSyntaxMacrosTestSupport"]),
     .binaryTarget(
         name: "SwiftSyntaxMacrosTestSupport",
         url: "https://github.com/johnno1962/InstantSyntax/raw/main/509.1.1/SwiftSyntaxMacrosTestSupport.framework.xcframework.zip",
-        checksum: "dcd78ad1553f53db6d9c02eeb55810a3c22ebea0fc1cd5bce1435235c1dd8c22"
+        checksum: "a4ced83b4f5ee62ff74875f8488205f58c122c91a7df0aeeed20001b6b4194d0"
     ),
     // .library(name: "_SwiftSyntaxTestSupport", targets: ["_SwiftSyntaxTestSupport"]),
     .binaryTarget(
         name: "_SwiftSyntaxTestSupport",
         url: "https://github.com/johnno1962/InstantSyntax/raw/main/509.1.1/_SwiftSyntaxTestSupport.framework.xcframework.zip",
-        checksum: "afac9280454858656434be8076a823fe32b6452b3ea744d116464660e8f90d91"
+        checksum: "fcc53aacf46fdb09d58feda9db5742d651b7fb6a74dc38612a26770796e5de1c"
     ),
   ]
 )
