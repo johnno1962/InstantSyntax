2bbba0d17473d18f23e9efe622cedcd17e544566
John Holdsworth <1786033+johnno1962@users.noreply.github.com>
2025-04-30 17:30:22 +0200

    601.0.1 compiled with Xcode 15.4 swift build.

--- Package.swift
+++ Package.swift
@@ -6,27 +6,27 @@ import PackageDescription
 let tag = "601.0.1" // swift-syntax version
 let archive = "https://github.com/swift-precompiled/swift-syntax/releases/download/"+tag
 let modules: [(name: String, checksum: String, depends: [String])] = [
-  ("SwiftBasicFormat", "2c498972cdb297a440b7c962cc05396fd4860d60de45d88e7fa7544e6c167077", ["SwiftSyntax509"]),
-  ("SwiftCompilerPlugin", "5a51dbb55b212f66747bba8713b2178b0cab46b5d160a1d3f471209a6654e51f", ["SwiftCompilerPluginMessageHandling", "SwiftSyntaxMacroExpansion", "SwiftSyntaxMacros", "SwiftSyntaxBuilder", "SwiftParserDiagnostics", "SwiftBasicFormat", "SwiftOperators", "SwiftParser", "SwiftDiagnostics", "SwiftSyntax", "SwiftSyntax509", "SwiftSyntax510", "SwiftSyntax600", "SwiftSyntax601", "_SwiftSyntaxCShims"]),
-  ("SwiftCompilerPluginMessageHandling", "ef395ccf6bbb18388a539a67ad94927c2a748d3b120dcc7cbfbb3e67a5cf3dc1", ["SwiftSyntaxMacroExpansion", "SwiftSyntaxMacros", "SwiftSyntaxBuilder", "SwiftParserDiagnostics", "SwiftBasicFormat", "SwiftOperators", "SwiftParser", "SwiftDiagnostics", "SwiftSyntax", "SwiftSyntax509"]),
-  ("SwiftDiagnostics", "0ca8f91e0c0b53905da9d82f2e69d01d2f6b48c252e1775730a74a3cd0354760", ["SwiftSyntax509"]),
-  ("SwiftIDEUtils", "f4b1df344d77851b85a8c21310101e37779efb90bb3ae5d318f234496b25d4f4", ["SwiftSyntax509"]),
-  ("SwiftIfConfig", "441f4f96f3e227a3730fb1036b3120b7ea1792fd0e59d65207fa204de175d445", ["SwiftSyntax509"]),
-  ("SwiftLexicalLookup", "884acdaee11ac8121917f2d5544b898f2abed6396cf8dd596556a9457ebf319f", ["SwiftSyntax509"]),
-  ("SwiftOperators", "5a7ee39f80141c0ca6ad5f6c1360d59bbe7ccef674f7b499f7c88c81b87f981d", ["SwiftParser", "SwiftDiagnostics", "SwiftSyntax", "SwiftSyntax509"]),
-  ("SwiftParser", "cb0217a1dbcb506490b7918cdf2114c731773201b469a336d9af1930613e7791", ["SwiftSyntax", "SwiftSyntax509"]),
-  ("SwiftParserDiagnostics", "f7f02971975d9d0ed4a4a38b57e221bf3d5fd4fdd71d9b38a21761b69df95d4a", ["SwiftParser", "SwiftDiagnostics", "SwiftBasicFormat", "SwiftSyntax", "SwiftSyntax509"]),
-  ("SwiftRefactor", "6fb5bbee91ac086def24a263bde594c215f6061fbc5aecad89ed0a7e669884b3", ["SwiftSyntax509"]),
-  ("SwiftSyntax", "23a3bde9b8ecf5514e8fd813652b30be6f1ecc3542d398486b1c8fc2ff893b78", ["SwiftSyntax509"]),
-  ("SwiftSyntax509", "623694ffbbf0db0f4449627922f1d2b362005068049261711eb35732bf770751", []),
-  ("SwiftSyntax510", "286beafe90a8b5d1bf38d1bc81d99f8c0925f5df3fdb43b3ea27947ab37dadae", []),
-  ("SwiftSyntax600", "9fe071ba080666feb341f4f22379bc70243e17664e13e12284516f836fd9bc19", []),
-  ("SwiftSyntax601", "24c4e8357ef8d7891d86a12ef2c85b0399fa7c37a4c434b85f775b415582e2e3", []),
-  ("_SwiftSyntaxCShims", "1c620be31b51590e9420668d13d45b197aad28c0a53d73dec30274c40cd4d993", []),
-  ("SwiftSyntaxBuilder", "95101b35e0735fc839794fb601607c741c003c912420b7bf4b23dd81b1fa9e19", ["SwiftParserDiagnostics", "SwiftDiagnostics", "SwiftParser", "SwiftBasicFormat", "SwiftSyntax", "SwiftSyntax509"]),
-  ("SwiftSyntaxMacroExpansion", "58629356b78b4486047f8efbba9dc05fd7174d8b01171078771628f0269959eb", ["SwiftOperators", "SwiftSyntaxMacros", "SwiftSyntaxBuilder", "SwiftParserDiagnostics", "SwiftDiagnostics", "SwiftParser", "SwiftBasicFormat", "SwiftSyntax", "SwiftSyntax509"]),
-  ("SwiftSyntaxMacros", "f12a8bd28801b45a07c2df8f0961bf791135af39b8ce8283e9cb9ad181c49fab", ["SwiftSyntaxBuilder", "SwiftParserDiagnostics", "SwiftSyntax", "SwiftSyntax509"]),
-  ("SwiftSyntaxMacrosTestSupport", "acbc76f207113bc8f887c265ab4dfb04b031c794088eb4eef09e7c0bde971332", [/*"_SwiftSyntaxTestSupport", */"SwiftSyntaxMacroExpansion", "SwiftOperators", "SwiftSyntaxMacros", "SwiftSyntaxBuilder", "SwiftParserDiagnostics", "SwiftDiagnostics", "SwiftParser", "SwiftBasicFormat", "SwiftSyntax", "SwiftSyntax509"]),
+  ("SwiftBasicFormat", "10db9c2ea6f695f3dd00ed6e0dbad3696f7eee281d4813b49888c8acca0589c1", ["SwiftSyntax509"]),
+  ("SwiftCompilerPlugin", "67abbac29711a903c843bcaa74ac7d859497832b74d51e72980ee2d64fdf6de9", ["SwiftCompilerPluginMessageHandling", "SwiftSyntaxMacroExpansion", "SwiftSyntaxMacros", "SwiftSyntaxBuilder", "SwiftParserDiagnostics", "SwiftBasicFormat", "SwiftOperators", "SwiftParser", "SwiftDiagnostics", "SwiftSyntax", "SwiftSyntax509", "SwiftSyntax510", "SwiftSyntax600", "SwiftSyntax601", "_SwiftSyntaxCShims"]),
+  ("SwiftCompilerPluginMessageHandling", "5bca868bd7bc7704b8f38e0a0a17ce829f2eefac6be0580380ac51a191cf23b5", ["SwiftSyntaxMacroExpansion", "SwiftSyntaxMacros", "SwiftSyntaxBuilder", "SwiftParserDiagnostics", "SwiftBasicFormat", "SwiftOperators", "SwiftParser", "SwiftDiagnostics", "SwiftSyntax", "SwiftSyntax509"]),
+  ("SwiftDiagnostics", "3c434e7e6092a25bf738a4fdcbc19c43a9aca8456b13f0331ad638a7abf9c225", ["SwiftSyntax509"]),
+  ("SwiftIDEUtils", "04d41c581209643f6d3f524bdc3aa6604c160982bfe74d37eef7edd5f311cc9a", ["SwiftSyntax509"]),
+  ("SwiftIfConfig", "12962000d8b34a5de05fc2ef083a919e548590d3cf992f32c8b622cc90a76791", ["SwiftSyntax509"]),
+  ("SwiftLexicalLookup", "a18c4425960fd18d665b56a4bca9df65d501a65c578737cdd1a22a5ed2eb540b", ["SwiftSyntax509"]),
+  ("SwiftOperators", "20161bb2cb1c5b51cdc23114a0f30f60daa898a73df03db1ff4d776d5fa2c687", ["SwiftParser", "SwiftDiagnostics", "SwiftSyntax", "SwiftSyntax509"]),
+  ("SwiftParser", "829b2ac2a55b0bf51a656b54ea2a9d5763216b618082d259f4bacd735c224773", ["SwiftSyntax", "SwiftSyntax509"]),
+  ("SwiftParserDiagnostics", "a6a35ec74bbf0c46daa1b1c757cbdf4d241ff368b520b5127f50d21aeb096ff0", ["SwiftParser", "SwiftDiagnostics", "SwiftBasicFormat", "SwiftSyntax", "SwiftSyntax509"]),
+  ("SwiftRefactor", "a776fd2af50d85620944f389fadf98abaf4df2c90a5e44a0dbecdcd661a63b02", ["SwiftSyntax509"]),
+  ("SwiftSyntax", "750fc062f3f431e58f3afc0d1f664f51191f6f6c9982a4b6a949ddad1c970bc7", ["SwiftSyntax509"]),
+  ("SwiftSyntax509", "c51863bb51cbc23f3c2faff5616346bf07235a5ac775f579dbfb4262409f7a31", []),
+  ("SwiftSyntax510", "f0381cd6b0ca2a0340d8fe8a0ca8e557fd7214481f4058e212ac0252b631463f", []),
+  ("SwiftSyntax600", "d47864c2898fc0c38fa770574a02c287b3125858771e0100b5f29378fef6dca4", []),
+  ("SwiftSyntax601", "c00bc8c61b8e30167a7bc9e15023a2ad695c9decb1efdb2fb65b5cc4bc6c50ec", []),
+  ("SwiftSyntaxBuilder", "ca7213037eebdc26d64f0db8ce6679a0be65fa39c492ceb6c3d0b79559734437", ["SwiftParserDiagnostics", "SwiftDiagnostics", "SwiftParser", "SwiftBasicFormat", "SwiftSyntax", "SwiftSyntax509"]),
+  ("SwiftSyntaxMacroExpansion", "e230a8d4aae9ed9f4ff5ba1cfa94f72027ec9015daef951c16451945481a16a1", ["SwiftOperators", "SwiftSyntaxMacros", "SwiftSyntaxBuilder", "SwiftParserDiagnostics", "SwiftDiagnostics", "SwiftParser", "SwiftBasicFormat", "SwiftSyntax", "SwiftSyntax509"]),
+  ("SwiftSyntaxMacros", "37cd64e8ccb141a5ff860540107e51124cf0faf4435656588268f7593946e8ec", ["SwiftSyntaxBuilder", "SwiftParserDiagnostics", "SwiftSyntax", "SwiftSyntax509"]),
+  ("SwiftSyntaxMacrosTestSupport", "f8f7df797f6c3f389528d8c95389c06184ce0ee7a1bf2a865e2fbf3fc53d9de6", [/*"_SwiftSyntaxTestSupport", */"SwiftSyntaxMacroExpansion", "SwiftOperators", "SwiftSyntaxMacros", "SwiftSyntaxBuilder", "SwiftParserDiagnostics", "SwiftDiagnostics", "SwiftParser", "SwiftBasicFormat", "SwiftSyntax", "SwiftSyntax509"]),
+  ("_SwiftSyntaxCShims", "631bc3cd7edb64840fb6727c010587e18c2178d3a1f6617d2a6329b762889ca9", []),
 //  ("_SwiftSyntaxTestSupport", ["SwiftSyntaxMacroExpansion", "SwiftOperators", "SwiftSyntaxMacros", "SwiftSyntaxBuilder", "SwiftParserDiagnostics", "SwiftDiagnostics", "SwiftParser", "SwiftBasicFormat", "SwiftSyntax", "SwiftSyntax509"]),
 //  ("_SwiftCompilerPluginMessageHandling", []),
 //  ("_SwiftLibraryPluginProvider", []),
@@ -48,8 +48,7 @@ let package = Package(
   targets: modules.map {
       .binaryTarget(
           name: $0.name,
-          url: archive+"/\($0.name).xcframework.zip",
-          checksum: $0.checksum
+          path: tag + "/" + "\($0.name).xcframework.zip"
       )
   }
 )
---
+++ package_syntax.sh
@@ -0,0 +1,140 @@
+#!/bin/bash -x
+#
+# DIY rebuild of all binary framework zip files from Apple source.
+# This script takes about half an hour to run through.
+#
+# Make a fork of https://github.com/johnno1962/InstantSyntax
+# clone it and run this script inside the clone.
+#
+
+export REPO=${1:-https://github.com/apple/swift-syntax}
+export REPO_NAME=`basename "$REPO"`
+export TAG=${2:-601.0.1}
+export DEST="$PWD/$TAG"
+export SOURCE="/tmp/$REPO_NAME"
+export XCODED=`xcode-select -p`
+
+if [ -f Package.swift ]; then
+    export MANIFEST="$PWD/Package.swift.generated"
+else
+    export MANIFEST="$PWD/Package.swift"
+fi
+
+if [ ! -d "$SOURCE" ]; then
+  git clone "$REPO" "$SOURCE"
+fi
+
+mkdir -p $DEST &&
+cd $SOURCE &&
+git stash &&
+git checkout $TAG &&
+
+git apply <<RENAME
+index 10adca69..52310baf 100644
+--- a/Package.swift
++++ b/Package.swift
+@@ -40,7 +40,11 @@ if buildDynamicLibrary {
+     .library(name: "SwiftSyntaxMacroExpansion", targets: ["SwiftSyntaxMacroExpansion"]),
+     .library(name: "SwiftSyntaxMacrosTestSupport", targets: ["SwiftSyntaxMacrosTestSupport"]),
+     .library(name: "SwiftSyntaxMacrosGenericTestSupport", targets: ["SwiftSyntaxMacrosGenericTestSupport"]),
+-    .library(name: "_SwiftCompilerPluginMessageHandling", targets: ["SwiftCompilerPluginMessageHandling"]),
++    .library(name: "SwiftSyntax509", targets: ["SwiftSyntax509"]),
++    .library(name: "SwiftSyntax510", targets: ["SwiftSyntax510"]),
++    .library(name: "SwiftSyntax600", targets: ["SwiftSyntax600"]),
++    .library(name: "SwiftSyntax601", targets: ["SwiftSyntax601"]),
++    .library(name: "SwiftCompilerPluginMessageHandling", targets: ["SwiftCompilerPluginMessageHandling"]),
+     .library(name: "_SwiftLibraryPluginProvider", targets: ["SwiftLibraryPluginProvider"]),
+   ]
+ }
+RENAME
+
+export MODULES=`cd Sources; echo {Swift,_}*; cd VersionMarkerModules; echo *`
+export PLATFORMS="${3:-macOS iOS iOS_Simulator tvOS_Simulator}"
+export CONDITIONS="RESILIENT_LIBRARIES"
+export PARALLEL_BUILDS=4
+export CONFIG=Release
+
+ln -sf $CONFIG macos-arm64_x86_64 &&
+ln -sf $CONFIG-iphoneos ios-arm64 &&
+ln -sf $CONFIG-iphonesimulator ios-arm64_x86_64-simulator &&
+ln -sf $CONFIG-appletvsimulator tvos-arm64_x86_64-simulator &&
+
+cat <<PACKAGE >$MANIFEST
+// swift-tools-version: 5.9
+
+import CompilerPluginSupport
+import PackageDescription
+
+let tag = "$TAG" // $REPO version
+let modules: [(name: String, checksum: String, depends: [String])] = [
+PACKAGE
+
+cat <<'INNER' >/tmp/INNER.sh
+        PLATFORM=$1
+        DDATA="$SOURCE/build.$PLATFORM"
+        time $XCODED/usr/bin/xcodebuild archive -scheme $MODULE -quiet -configuration $CONFIG -destination "generic/platform=$(echo $PLATFORM | sed -e 's/_/ /g')" -archivePath $SOURCE/archives/$MODULE-$PLATFORM.xcarchive -derivedDataPath $DDATA SKIP_INSTALL=NO BUILD_LIBRARY_FOR_DISTRIBUTION=YES SWIFT_SERIALIZE_DEBUGGING_OPTIONS=NO SWIFT_ACTIVE_COMPILATION_CONDITIONS="$CONDITIONS" || exit 1
+INNER
+
+for MODULE in $MODULES; do
+    export MODULE
+    /bin/bash -x <<'OUTER'
+    LIBS=""
+    cd $SOURCE &&
+    echo $PLATFORMS | sed -e 's/ /\n/g' | xargs -P $PARALLEL_BUILDS -I % bash -x /tmp/INNER.sh % &&
+    cat <<PACKAGE >>$MANIFEST
+    ("$MODULE", "__CHECKSUM__${MODULE}__", ["$MODULE"]),
+PACKAGE
+
+    for PLATFORM in $PLATFORMS; do
+        DDATA="$SOURCE/build.$PLATFORM"
+        LIB="$DDATA/lib$MODULE.a"
+        rm -f $DDATA/lib$MODULE*.a &&
+        cd $DDATA/Build/Intermediates.noindex/ArchiveIntermediates/*/IntermediateBuildFilesPath/*.build/$CONFIG*/$MODULE.build/Objects-normal &&
+        for ARCH in *; do
+            ar qv $DDATA/lib$MODULE.$ARCH.a $ARCH/*.o &&
+            ranlib $DDATA/lib$MODULE.$ARCH.a
+        done && cd -
+        lipo -create $DDATA/lib$MODULE.*.a -output $LIB &&
+        LIBS="$LIBS -library $LIB"
+    done
+        
+    rm -rf $DEST/$MODULE.xcframework &&
+    $XCODED/usr/bin/xcodebuild -create-xcframework $LIBS -output $DEST/$MODULE.xcframework || exit 1
+
+    cd $DEST/$MODULE.xcframework && for VARIANT in *; do if [ $VARIANT != "Info.plist" ]; then
+        DIR=`readlink "$SOURCE/$VARIANT"`
+        cp -r $SOURCE/build.*/Build/Intermediates.noindex/ArchiveIntermediates/*/BuildProductsPath/$DIR/$MODULE.swiftmodule $VARIANT ||
+        cp -rf $SOURCE/build.*/Build/Intermediates.noindex/ArchiveIntermediates/*/BuildProductsPath/$DIR/SwiftSyntax509.swiftmodule $VARIANT/$MODULE.swiftmodule
+    fi done
+
+    cd $DEST && rm -f $MODULE.xcframework.zip $MODULE.xcframework/*/*/*.swiftmodule &&
+    codesign -f --timestamp -s "Developer ID Application" $MODULE.xcframework &&
+    (zip -r9 --symlinks "$MODULE.xcframework.zip" "$MODULE.xcframework" >>../../zips.txt; \
+     CHECKSUM=`swift package compute-checksum "$MODULE.xcframework.zip"`; \
+     for MANIFEST in $MANIFEST ../Package.swift; do \
+     sed -e "s/[(]\"$MODULE\", \"[^\"]*/(\"$MODULE\", \"$CHECKSUM/g" <$MANIFEST >$MANIFEST.$$ && \
+     mv -f $MANIFEST.$$ $MANIFEST; done) &
+OUTER
+done && sleep 10 && cat <<PACKAGE >>$MANIFEST && echo "Build complete."
+]
+
+let package = Package(
+  name: "$REPO_NAME",
+  platforms: [
+    .iOS(.v13),
+    .macOS(.v10_15),
+    .tvOS(.v13),
+    .watchOS(.v6),
+  ],
+  
+  products: modules.map {
+      .library(name: \$0.name, targets: [\$0.name] + \$0.depends
+    ) },
+
+  targets: modules.map {
+      .binaryTarget(
+          name: \$0.name,
+          path: tag + "/" + "\(\$0.name).xcframework.zip"
+    ) }
+)
+PACKAGE
